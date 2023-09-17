9ca3bedc746490dbe0f8c239884bcf18f10c8785
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-09-17 16:20:44 -0400

    Implementing a first pass of adding assignments to a course. Closes #17

--- App.Hobbits/Library.Hobbits/Models/Assignment.cs
+++ App.Hobbits/Library.Hobbits/Models/Assignment.cs
@@ -2,6 +2,9 @@
 {
     public class Assignment
     {
-
+        public string? Name { get; set; }
+        public string Description { get; set; }
+        public decimal TotalAvailablePoints { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
