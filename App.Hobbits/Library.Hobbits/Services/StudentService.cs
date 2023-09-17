f9bb179b003e4b52d38185fa03410901dc8e074f
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-09-17 15:32:38 -0400

    Adding singleton pattern for the student service and integrating that new pattern into CourseHelper and StudentHelper. Closes #8

--- App.Hobbits/Library.Hobbits/Services/StudentService.cs
+++ App.Hobbits/Library.Hobbits/Services/StudentService.cs
@@ -5,7 +5,27 @@ namespace Library.Hobbits.Services
 {
     public class StudentService
     {
-        private List<Person> studentList = new List<Person>();
+        private List<Person> studentList;
+
+        private static StudentService? _instance;
+
+        public StudentService() 
+        { 
+            studentList = new List<Person>();
+        }
+
+        public static StudentService Current
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new StudentService();
+                }
+
+                return _instance;
+            }
+        }
  
         public void Add(Person student) 
         { 
