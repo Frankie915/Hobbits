548405ea732d48f4a049dfcc9964d7466affa604
Jonathan Diaz <101024433+BACPoox@users.noreply.github.com>
2023-09-27 11:42:11 -0400

    Added AssignmentItem. Closes #32

---
+++ App.Hobbits/Library.Hobbits/Models/AssignmentItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Library.Hobbits.Models
+{
+    public class AssignmentItem : ContentItem
+    {
+        public Assignment? Assignment { get; set; }
+    }
+}
+
