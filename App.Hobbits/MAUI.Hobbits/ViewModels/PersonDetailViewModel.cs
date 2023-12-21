7968656b4582d6200834255de7c3d81286352403
Jonathan Diaz <jrd17j@fsu.edu>
2023-12-21 15:59:59 -0500

    Added BindingContext reset on navigating to and from the person detail view. Closes #94

--- App.Hobbits/MAUI.Hobbits/ViewModels/PersonDetailViewModel.cs
+++ App.Hobbits/MAUI.Hobbits/ViewModels/PersonDetailViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Hobbits.Models;
+using Library.Hobbits.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,28 @@ namespace MAUI.Hobbits.ViewModels
     {
         public string Name { get; set; }
         public string ClassificationString { get; set; }
+
+        public void AddPerson()
+        {
+            PersonClassification classification;
+            switch (ClassificationString)
+            {
+                case "S":
+                    classification = PersonClassification.Senior;
+                    break;
+                case "J":
+                    classification = PersonClassification.Junior;
+                    break;
+                case "O":
+                    classification = PersonClassification.Sophomore;
+                    break;
+                case "F":
+                default:
+                    classification = PersonClassification.Senior;
+                    break;
+            }
+            StudentService.Current.Add(new Student { Name = Name, Classification = classification });
+            Shell.Current.GoToAsync("//Instructor");
+        }
     }
 }
