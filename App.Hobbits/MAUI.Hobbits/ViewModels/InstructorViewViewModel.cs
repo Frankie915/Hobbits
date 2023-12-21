9fba34170d2f9ef1160f7fa533ef584b9b9dc289
Jonathan Diaz <jrd17j@fsu.edu>
2023-12-21 10:49:00 -0500

    Added listing functionality to instructor view.

--- App.Hobbits/MAUI.Hobbits/ViewModels/InstructorViewViewModel.cs
+++ App.Hobbits/MAUI.Hobbits/ViewModels/InstructorViewViewModel.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Library.Hobbits.Models;
+using Library.Hobbits.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MAUI.Hobbits.ViewModels
 {
-    internal class InstructorViewViewModel
+    public class InstructorViewViewModel: INotifyPropertyChanged
     {
+        public ObservableCollection<Person> People
+        {
+            get
+            {
+                return new ObservableCollection<Person>(StudentService.Current.Students);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddClick(Shell s)
+        {
+            s.GoToAsync("//PersonDetail");
+        }
+
+        public void RefreshView()
+        {
+            NotifyPropertyChanged(nameof(People));
+        }
     }
 }
